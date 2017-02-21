533718d65723dc02fd24799da109c5c769261c39
monkingxue <1360662499@qq.com>
2017-02-21 12:48:46 +0800

    增加 postcss ，改进 config

---
+++ src/redux/store/syncHistoryWithStore.js
@@ -0,0 +1,19 @@
+// ========================================================
+// 同步 history 配置
+// ========================================================
+import { browserHistory } from "react-router";
+import { routerMiddleware, syncHistoryWithStore } from "react-router-redux";
+
+export const historyMiddleware = routerMiddleware(browserHistory);
+
+/**
+ * @param  {store} store 实例
+ * @return {History} 增强版 history
+ */
+export default function (store) {
+  return syncHistoryWithStore(
+    browserHistory,
+    store,
+    { selectLocationState: (state) => state.router }
+  );
+}
---
+++ src/utils/dateTimeFormatter.js
@@ -0,0 +1,44 @@
+/**
+ * 简易的left-pad
+ * @param {Number} lpad 需要填充的字符
+ * @param {Number} bit 填充的位数
+ * @return {function}
+ */
+const prefix = (lpad, bit) => (val) => (lpad.toString().repeat(bit) + val).slice(-bit);
+
+
+/**
+ * 个位数前加零
+ * @param  {Number} val
+ * @return {String/Number}
+ */
+const zerofill = prefix("0", 2);
+
+/**
+ * 格式化时间
+ * @param  {Number} time 时间戳
+ * @param  {Number} type 格式化类型
+ * @return {String}
+ */
+export default function dataTimeFormatter(time, type) {
+  let date = new Date(time);
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let second = date.getSeconds();
+
+  switch (type) {
+    case 0: // 01-05
+      return `${zerofill(month)}-${zerofill(day)}`;
+    case 1: // 11:12
+      return `${zerofill(hours)}-${zerofill(minutes)}`;
+    case 2: // 2015-01-05
+      return `${year}-${zerofill(month)}-${zerofill(day)}`;
+    case 3: // 2015-01-05 11:12
+      return `${year}-${zerofill(month)}-${zerofill(day)}  ${zerofill(hours)}:${zerofill(minutes)}`;
+    default: // 2015-01-05 11:12:13
+      return `${year}-${zerofill(month)}-${zerofill(day)}  ${zerofill(hours)}:${zerofill(minutes)}:${zerofill(second)}`;
+  }
+}
